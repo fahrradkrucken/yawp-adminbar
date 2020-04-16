e57cee915b2397bc092c1ad65b94b1996bc4c0cb
fahrradkrucken <sebastians@ukr.net>
2020-04-16 21:21:04 +0300

    Initial TODOs

--- src/YAWPAdminBar.php
+++ src/YAWPAdminBar.php
@@ -3,6 +3,16 @@
 
 class YAWPAdminBar
 {
+    private $settingsToggle = [
+        'class'      => '',
+        'position_x' => 'left',
+        'position_y' => 'bottom',
+    ];
+    private $settingsMenuItems = [
+        'class' => '',
+        'items' => [],
+    ];
+
     public function bootstrap()
     {
         $this->registerHooks();
@@ -12,16 +22,32 @@ class YAWPAdminBar
 
     private function registerHooks()
     {
-
+        // @TODO: hooks to replace / add items
     }
 
     private function initDefaults()
     {
-
+        // @TODO: pass default arguments & menu items through hooks
     }
 
     private function enqueueScripts()
     {
+        // @TODO: enqueue to header "adminbar.css" and "dashicons"
+        // @TODO: enqueue to footer adminbar HTML markup
+    }
 
+    public function renderToggle($settings = [])
+    {
+        // @TODO: render toggle button
+    }
+
+    public function renderMenu($settings = [])
+    {
+        // @TODO: render menu items box
+    }
+
+    public function renderMenuItems($items = [])
+    {
+        // @TODO: render menu items list
     }
 }
